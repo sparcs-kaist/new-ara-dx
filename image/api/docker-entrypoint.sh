96b715922052bb5a48b730c5429b08597008f805
triangleyj <dpwns99@gmail.com>
2022-12-01 15:57:30 +0000

    Fix elastic test failing, make i18n automatically

--- image/api/docker-entrypoint.sh
+++ image/api/docker-entrypoint.sh
@@ -3,6 +3,9 @@ echo ====File move start====;
 if ! [ "$(ls -A /root/api/apps)" ]; then
     echo Directory is empty: copy src, run initialize
     cp -a /tmp/newara/www/root/. /root/
+    # For elasticsearch, below synonym is volume-mapped
+    # It generate empty synonym file, and git clone failed.
+    rm -rf /root/api/ara
     git clone https://github.com/sparcs-kaist/new-ara-api.git /root/api/.
     cat /tmp/env/firebaseServiceAccountKey.json > /root/api/firebaseServiceAccountKey.json;
     git fetch;
@@ -17,6 +20,8 @@ if ! [ "$(ls -A /root/api/apps)" ]; then
 	sleep 1
     done
     make migrate;
+    make i18n_compile;
+    make i18n_generate;
     # if all tasks done, then create .env so that allow to do make run
     echo -e "\n"|ssh-keygen -t rsa -N ""
     touch /root/.ssh/authorized_keys
